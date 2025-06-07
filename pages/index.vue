7ae8e8eebe502d616d7f55577ab5491420143d1a
louis-guidez <louisguidez3@gmail.com>
2025-06-07 17:04:46 +0200

    feat: visuel des CRUDs

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<!-- pages/index.vue -->
+<template>
+  <div>
+    <h1>Utilisateurs</h1>
+    <ul>
+      <li v-for="user in users" :key="user.id">
+        {{ user.name }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+const { data: users } = await useAsyncData('users', () => $fetch('http://localhost:8000/api/users'))
+</script>
---
+++ pages/photos.vue
@@ -0,0 +1,17 @@
+<!-- pages/index.vue -->
+<template>
+  <div>
+    <h1>Photos</h1>
+    <ul>
+      <li v-for="photo in photos" :key="photo.id">
+        {{ photo.id }}
+        {{ photo.urlChemin }}
+        {{ photo.dateUpload }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+const { data: photos } = await useAsyncData('photos', () => $fetch('http://localhost:8000/api/photos'))
+</script>
