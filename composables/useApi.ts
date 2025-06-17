fb739219d91f9b490ec4c0b948755855a1907bbd
louis-guidez <louisguidez3@gmail.com>
2025-06-17 17:01:44 +0200

    fix: correction useApi.ts

--- composables/useApi.ts
+++ composables/useApi.ts
@@ -1,5 +1,4 @@
-export const baseURL = useRuntimeConfig().public.API_BASE_URL
-
 export function apiFetch<T>(path: string, options?: Parameters<typeof $fetch<T>>[1]) {
+  const baseURL = useRuntimeConfig().public.API_BASE_URL
   return $fetch<T>(`${baseURL}${path}`, options)
 }
