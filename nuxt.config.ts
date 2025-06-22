49da080392491df6e21ee273459ada7f55e693ef
louis-guidez <louisguidez3@gmail.com>
2025-06-22 12:03:12 +0200

    feat: dockerisation

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,7 +11,8 @@ export default defineNuxtConfig({
   modules: ['@nuxt/eslint'],
   runtimeConfig: {
     public: {
-      API_BASE_URL: process.env.API_BASE_URL || 'http://localhost:8000/api',
+      // API_BASE_URL: process.env.API_BASE_URL || 'http://localhost:8000/api',
+      API_BASE_URL: process.env.API_BASE_URL || 'http://nginx/api',
     },
   },
 })
