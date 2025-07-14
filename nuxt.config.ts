c231bb1e3a74a06e514b8d941a18b4461ede727b
Sioood <theodupontpro@gmail.com>
2025-07-14 18:42:53 +0200

    feat: :sparkles: add place autocomplete input

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,7 +12,9 @@ export default defineNuxtConfig({
   runtimeConfig: {
     public: {
       API_BASE_URL: process.env.API_BASE_URL || 'http://localhost:8000/api',
+      // TODO need to move this to private variable => only accessible on the server-side
       STRIPE_KEY: process.env.NUXT_PUBLIC_STRIPE_KEY,
+      GEOAPIFY_API_KEY: process.env.GEOAPIFY_API_KEY,
     },
   },
   i18n: {
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,7 @@
 <template>
   <div>
     <h1>Index</h1>
+    <UiInputPlaceAutocomplete />
+    <UiInput />
   </div>
 </template>
