aa40a24e8f0e623c04206520326454ba5c106246
louis-guidez <louisguidez3@gmail.com>
2025-07-19 21:26:56 +0200

    fix: index.vue affichage des annonces via bdd

--- components/Ui/AnnonceCard.vue
+++ components/Ui/AnnonceCard.vue
@@ -1,5 +1,7 @@
 <script setup>
-defineProps({
+import { computed } from 'vue'
+
+const props = defineProps({
   annonce: {
     type: Object,
     default: () => ({}),
@@ -8,16 +10,35 @@ defineProps({
 })
 
 defineEmits(['toggle-favorite'])
+
+const location = computed(() => {
+  return ''
+})
+
+const seller = computed(() => {
+  const email = props.annonce?.user?.email
+  if (typeof email !== 'undefined') return `${email}`
+  return ''
+})
+
+// const seller = computed(() => {
+//   const prenom = props.annonce?.utilisateur?.prenom
+//   const email = props.annonce?.utilisateur?.email
+//   if (prenom && typeof note !== 'undefined') return `${prenom} - ${email}`
+//   if (prenom) return prenom
+//   if (typeof email !== 'undefined') return `${email}`
+//   return ''
+// })
 </script>
 
 <template>
   <div class="card">
-    <img :src="annonce.photos[0]" alt="photo" class="card-img" />
+    <img :src="annonce.photos?.[0]" alt="photo" class="card-img" />
     <div class="card-body">
       <div class="card-title">{{ annonce.titre }}</div>
-      <div class="card-location">Cambrai - - 30 km</div>
+      <div class="card-location">{{ location }}</div>
       <div class="card-footer">
-        <div class="card-user">Marc - 4.89</div>
+        <div class="card-user">{{ seller }}</div>
         <div class="card-price">{{ annonce.prix }}€</div>
       </div>
     </div>
