663503987b696959fbd64755a85af1c960cab5de
Sioood <theodupontpro@gmail.com>
2025-07-19 17:29:30 +0200

    style: :lipstick: top navbar link style

--- components/Ui/TopNavbar.vue
+++ components/Ui/TopNavbar.vue
@@ -23,15 +23,15 @@ defineExpose({ search, searchRef })
     </form>
 
     <div class="inline-flex gap-2 items-center justify-center">
-      <NuxtLink to="/CreateAnnonce" class="flex items-center gap-2">
-        <uiButton intent="primary" size="md" class="lg:block hidden">
+      <NuxtLink to="/CreateAnnonce" class="lg:flex hidden">
+        <uiButton intent="primary" size="md" class="flex items-center gap-2">
           <Icon :size="16" name="fluent:add-12-filled" />
           <span class="font-semibold leading-0">{{ $t('createAnAd') }}</span>
         </uiButton>
       </NuxtLink>
 
-      <NuxtLink :to="user ? '/profile' : '/login'" class="flex items-center gap-2">
-        <uiButton intent="ghost" class="!p-2">
+      <NuxtLink :to="user ? '/profile' : '/login'">
+        <uiButton intent="ghost" class="!p-2 flex items-center gap-2">
           <Icon :size="24" name="fluent:person-12-filled" />
         </uiButton>
       </NuxtLink>
