8263ad205871c762be8df44a7c46bd6f28c7c52d
Sioood <theodupontpro@gmail.com>
2025-07-20 15:47:38 +0200

    fix: :bug: use apiFectch and mercure in env

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,6 +12,7 @@ export default defineNuxtConfig({
   runtimeConfig: {
     public: {
       API_BASE_URL: process.env.API_BASE_URL || 'http://localhost:8000/api',
+      MERCURE_BASE_URL: process.env.API_BASE_URL || 'http://localhost:3001/.well-known/mercure',
       // TODO need to move this to private variable => only accessible on the server-side
       STRIPE_KEY: process.env.NUXT_PUBLIC_STRIPE_KEY,
       GEOAPIFY_API_KEY: process.env.GEOAPIFY_API_KEY,
