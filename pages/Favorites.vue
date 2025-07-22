ab9402ed3d8c66aa5e19f1b6a23318bf1fd0c434
Sioood <theodupontpro@gmail.com>
2025-07-22 11:01:03 +0200

    feat: :sparkles: improve annonce card and list

---
+++ pages/Favorites.vue
@@ -0,0 +1,35 @@
+<script setup>
+defineOgImageComponent('Lendo', {
+  headline: '🛒',
+  title: 'Lendo',
+  description: 'Lendo',
+  theme: 'green',
+  colorMode: 'dark',
+})
+
+onMounted(async () => {
+  await fetchAnnonces()
+})
+
+const favorites = useCookie('favorites', { default: () => [] })
+
+function toggleFavorite(annonce) {
+  const exists = favorites.value.find((a) => a.id === annonce.id)
+  if (exists) {
+    favorites.value = favorites.value.filter((a) => a.id !== annonce.id)
+  } else {
+    favorites.value.push(annonce)
+  }
+}
+</script>
+
+<template>
+  <div class="p-12 flex flex-col gap-12">
+    <section v-if="favorites.length" class="flex flex-col gap-4">
+      <h2 class="text-2xl font-semibold">Vos offres sauvegardées</h2>
+      <div class="grid grid-cols-[repeat(auto-fill,minmax(400px,0fr))] gap-4">
+        <UiAnnonceCard v-for="annonce in favorites" :key="annonce.id" :annonce="annonce" :is-favorite="true" @toggle-favorite="toggleFavorite" />
+      </div>
+    </section>
+  </div>
+</template>
