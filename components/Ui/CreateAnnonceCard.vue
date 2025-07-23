3675251beba1bdaec5f8114e8822a95708be9393
Sioood <theodupontpro@gmail.com>
2025-07-23 22:51:31 +0200

    feat: :sparkles: add create annonce card when category is empty

---
+++ components/Ui/CreateAnnonceCard.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="size-full overflow-hidden rounded-lg bg-green-100 aspect-[6/5] relative flex flex-col items-center justify-center gap-2">
+    <Icon name="fluent:add-12-filled" size="48" class="text-green-600" />
+    <span>
+      <b class="font-semibold">{{ $t('createAnAd') }}</b>
+    </span>
+  </div>
+</template>
