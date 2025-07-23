4c5bb98da8005655767a0156bbb786fd320d6518
Sioood <theodupontpro@gmail.com>
2025-07-23 13:37:50 +0200

    style: :lipstick: favorite annonce card width

--- pages/Favorites.vue
+++ pages/Favorites.vue
@@ -27,7 +27,7 @@ function toggleFavorite(annonce) {
   <div class="p-12 flex flex-col gap-12">
     <section v-if="favorites.length" class="flex flex-col gap-4">
       <h2 class="text-2xl font-semibold">Vos offres sauvegardées</h2>
-      <div class="grid grid-cols-[repeat(auto-fill,minmax(400px,0fr))] gap-4">
+      <div class="grid grid-cols-[repeat(auto-fill,minmax(300px,0fr))] gap-4">
         <UiAnnonceCard v-for="annonce in favorites" :key="annonce.id" :annonce="annonce" :is-favorite="true" @toggle-favorite="toggleFavorite" />
       </div>
     </section>
